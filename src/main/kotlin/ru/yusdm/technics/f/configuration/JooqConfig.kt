aafcb6a9ccb5b0ff23595892f7db2718f98072d3
dyusupov <dmitry.yusupov@infobip.com>
2020-09-17 09:51:59 +0300

    Added autogenerated files for jooq demo

--- src/main/kotlin/ru/yusdm/technics/f/configuration/JooqConfig.kt
+++ src/main/kotlin/ru/yusdm/technics/f/configuration/JooqConfig.kt
@@ -4,28 +4,43 @@ import org.jooq.SQLDialect
 import org.jooq.impl.DataSourceConnectionProvider
 import org.jooq.impl.DefaultConfiguration
 import org.jooq.impl.DefaultDSLContext
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.jooq.SpringTransactionProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy
+import org.springframework.transaction.TransactionManager
 import javax.sql.DataSource
+
+const val JOOQ_TX_MANAGER = "jooqTxManager"
 
 @Configuration
 class JooqConfig {
 
-  /*  @Bean
+    @Bean(name = [JOOQ_TX_MANAGER])
+    fun transactionManager(ds: DataSource) = DataSourceTransactionManager(ds)
+
+    @Bean
+    fun connectionProvider(ds: DataSource) =
+        DataSourceConnectionProvider(TransactionAwareDataSourceProxy(ds))
+
+    @Bean
+    fun txProvider(@Qualifier(JOOQ_TX_MANAGER) txMngr: DataSourceTransactionManager) =
+        SpringTransactionProvider(txMngr)
+
+    @Bean
     fun configuration(
-        dataSource: DataSource,
-        transactionManager: DataSourceTransactionManager
+        connectionProvider: DataSourceConnectionProvider,
+        txProvider: SpringTransactionProvider
     ) = DefaultConfiguration().apply {
         this.set(SQLDialect.POSTGRES)
-        this.setConnectionProvider(DataSourceConnectionProvider(TransactionAwareDataSourceProxy(dataSource)))
-        this.setTransactionProvider(SpringTransactionProvider(transactionManager))
+        this.setConnectionProvider(connectionProvider)
+        this.setTransactionProvider(txProvider)
     }
 
     @Bean
-    fun dsl(configuration: DefaultConfiguration) = DefaultDSLContext(configuration)*/
+    fun dsl(configuration: DefaultConfiguration) = DefaultDSLContext(configuration)
 }
 
 
